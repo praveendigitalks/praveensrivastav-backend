37a808e5722a1580c2fcc653776f121551d04895
praveendigitalks <praveendigitalks@gmail.com>
2026-03-13 16:34:07 +0530

    added authorization

--- routes/portfolio.routes.js
+++ routes/portfolio.routes.js
@@ -8,18 +8,20 @@ import {MODULES} from "./../constants/module.js";
 import {ACTIONS} from "./../constants/permission.js";
 import { uploadPortfolioImage } from "../midleware/uploadproject.middleware.js";
 
-// router.post("/",checkPermission(MODULES.PORTFOLIO,ACTIONS.CREATE),createController);
-// router.get("/", checkPermission(MODULES.PORTFOLIO,ACTIONS.READ),getController);
-// router.get("/:id",checkPermission(MODULES.PORTFOLIO,ACTIONS.READ), getByIdController);
-// router.put("/:id",checkPermission(MODULES.PORTFOLIO,ACTIONS.UPDATE), updateController );
-// router.delete("/:id",checkPermission(MODULES.PORTFOLIO,ACTIONS.DELETE), deleteController);
 router.get("/categories", getCategoriesController);
-router.post("/",uploadPortfolioImage.single("projectImage"),createController);
-router.get("/",getController);
-router.get("/:id", getByIdController);
-router.put("/:id",uploadPortfolioImage.single("projectImage"), updateController );
-router.delete("/:id", deleteController);
+router.post("/",checkPermission(MODULES.PORTFOLIO,ACTIONS.CREATE),createController);
+router.get("/", checkPermission(MODULES.PORTFOLIO,ACTIONS.READ),getController);
+router.get("/:id",checkPermission(MODULES.PORTFOLIO,ACTIONS.READ), getByIdController);
+router.put("/:id",checkPermission(MODULES.PORTFOLIO,ACTIONS.UPDATE), updateController );
+router.delete("/:id",checkPermission(MODULES.PORTFOLIO,ACTIONS.DELETE), deleteController);
 router.get("/category/:category", getByCategoryController);
+
+
+// router.post("/",uploadPortfolioImage.single("projectImage"),createController);
+// router.get("/",getController);
+// router.get("/:id", getByIdController);
+// router.put("/:id",uploadPortfolioImage.single("projectImage"), updateController );
+// router.delete("/:id", deleteController);
 
 
 export default router;
